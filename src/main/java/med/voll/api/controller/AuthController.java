434681629ee76673255ff97fe4673778bf5250c2
maxc0d3 <maximitianoreste@gmail.com>
2023-09-09 21:11:31 -0500

    jwt implemented

--- src/main/java/med/voll/api/controller/AuthController.java
+++ src/main/java/med/voll/api/controller/AuthController.java
@@ -1,16 +1,18 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.users.User;
 import med.voll.api.domain.users.records.DataAuthUser;
-  import org.springframework.beans.factory.annotation.Autowired;
-  import org.springframework.http.ResponseEntity;
-  import org.springframework.security.authentication.AuthenticationManager;
-  import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-  import org.springframework.security.core.Authentication;
-  import org.springframework.web.bind.annotation.PostMapping;
-  import org.springframework.web.bind.annotation.RequestBody;
-  import org.springframework.web.bind.annotation.RequestMapping;
-  import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.infrastructure.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
@@ -18,13 +20,17 @@ public class AuthController {
 
   @Autowired
   private AuthenticationManager authenticationManager;
+  @Autowired
+  private TokenService tokenService;
 
   @PostMapping
-  private ResponseEntity<?> login(@RequestBody @Valid DataAuthUser dataAuthUser){
-    Authentication token = new UsernamePasswordAuthenticationToken(dataAuthUser.login(), dataAuthUser.password());
-    authenticationManager.authenticate(token);
+  private ResponseEntity<?> login(@RequestBody @Valid DataAuthUser dataAuthUser) {
+    Authentication authToken = new UsernamePasswordAuthenticationToken(dataAuthUser.login(), dataAuthUser.password());
+    var authUser = authenticationManager.authenticate(authToken);
 
-    return ResponseEntity.ok().build();
+    var JWTtoken = tokenService.generateToken((User) authUser.getPrincipal());
+
+    return ResponseEntity.ok(JWTtoken);
   }
 
 }
